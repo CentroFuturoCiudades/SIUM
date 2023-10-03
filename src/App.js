d4e1ac546104e0d66394c409e1e557957f208dad
urielsu96 <uriels@tec.mx>
2023-10-03 12:35:12 -0600

    First Commit - Scroll navigation with cards and DeckGL added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,206 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import DeckGL from "@deck.gl/react";
+import { GeoJsonLayer } from "@deck.gl/layers";
+import StaticMap from "react-map-gl";
+import maplibregl from "maplibre-gl";
 
-function App() {
+import "maplibre-gl/dist/maplibre-gl.css";
+import styles from "./styles.module.css";
+
+const DECK_GL_CONTROLLER = {
+  scrollZoom: false,
+  touchZoom: true,
+  keyboard: { moveSpeed: false },
+  dragMode: "pan",
+  inertia: true,
+};
+const INITIAL_STATE = {
+  latitude: 25.652983,
+  longitude: -100.286419,
+  zoom: 10,
+  transitionDuration: 800,
+  pitch: 0,
+  bearing: 0,
+};
+const PERIFERIES = [
+  "Juárez",
+  "García",
+  "Apodaca",
+  "Gral. Escobedo",
+  "Santiago",
+  "Ciénega de Flores",
+  "Salinas Victoria",
+  "Pesquería",
+  "Carmen",
+  "Cadereyta Jiménez",
+  "Hidalgo",
+  "Gral. Zuazua",
+];
+const SUBCENTERS = [
+  "Monterrey",
+  "San Pedro Garza García",
+  "San Nicolás de los Garza",
+  "Guadalupe",
+  "Santa Catarina",
+];
+const PERIFERY_LAYER = {
+  id: "perfifery-layer",
+  data: "data/municipalities.json",
+  dataTransform: (d) =>
+    d.features.filter((x) => PERIFERIES.includes(x.properties.NOMBRE)),
+  stroked: true,
+  filled: true,
+  lineWidthScale: 10,
+  lineWidthMinPixels: 2,
+  getFillColor: [255, 174, 0, 10],
+  getLineColor: [255, 174, 0, 200],
+  getLineWidth: 10,
+};
+const CENTER_LAYER = {
+  id: "center-layer",
+  data: "data/municipalities.json",
+  dataTransform: (d) =>
+    d.features.filter((x) => SUBCENTERS.includes(x.properties.NOMBRE)),
+  stroked: true,
+  filled: true,
+  lineWidthScale: 10,
+  lineWidthMinPixels: 2,
+  getFillColor: [7, 3, 252, 10],
+  getLineColor: [7, 3, 252, 200],
+  getLineWidth: 10,
+};
+
+function Card({ id, children }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section id={id}>
+      <div className={`${styles.card}`}>{children}</div>
+    </section>
   );
 }
 
-export default App;
+function Card1({ setOutline }) {
+  return (
+    <Card id="section1">
+      <h2 className={styles.title}>Title 1</h2>
+      Las familias migran a la{" "}
+      <span
+        className={styles.highlightAccent}
+        onMouseOver={() => setOutline(PERIFERY_LAYER)}
+        onMouseOut={() => setOutline(null)}
+      >
+        periferia
+      </span>{" "}
+      donde hay menor acceso a transporte público, servicios y empleo.
+    </Card>
+  );
+}
+
+function Card2({ setOutline }) {
+  return (
+    <Card id="section2">
+      <h2 className={styles.title}>¿Por qué migramos a la periferia?</h2>
+      Porque en los{" "}
+      <span
+        className={styles.highlightPrimary}
+        onMouseOver={() => setOutline(CENTER_LAYER)}
+        onMouseOut={() => setOutline(null)}
+      >
+        subcentros
+      </span>{" "}
+      la vivienda es muy cara para gran parte de los ciudadanos.
+    </Card>
+  );
+}
+
+function Card3({ setOutline }) {
+  return (
+    <Card id="section3">
+      <h2 className={styles.title}>Title 3</h2>
+      <p>
+        Alrededor del <b>40%</b> de los traslados se hacen desde la{" "}
+        <span
+          className={styles.highlightAccent}
+          onMouseOver={() => setOutline(PERIFERY_LAYER)}
+          onMouseOut={() => setOutline(null)}
+        >
+          periferia
+        </span>{" "}
+        como Apodaca, Escobedo, García y Juárez y el <b>26%</b> se traslada a
+        Monterrey.
+      </p>
+      <p>
+        El <b>45%</b> de los viajes son hechos al trabajo.
+      </p>
+      <p>
+        El <b>47%</b> de los viajes son hechos en automóvil, donde más de la
+        mitad <b>viajan solos</b>.
+      </p>
+      <p>
+        En promedio se invierten <b>68 minutos</b> en viaje redondo.
+      </p>
+    </Card>
+  );
+}
+
+export default function App() {
+  const [viewState, setViewState] = useState(INITIAL_STATE);
+  const [outline, setOutline] = useState(null);
+
+  useEffect(() => {
+    const isSectionInView = (section) => {
+      const { top, bottom } = section.getBoundingClientRect();
+      const midScreen = window.innerHeight * 0.5;
+
+      return (
+        (top > 0 && top <= midScreen) ||
+        (bottom > midScreen && bottom <= window.innerHeight)
+      );
+    };
+
+    const getCurrentSectionId = () => {
+      const sections = document.querySelectorAll("section");
+      const currentSection = Array.from(sections).find(isSectionInView);
+
+      return currentSection ? currentSection.id : null;
+    };
+
+    const handleScroll = () => {
+      const currentSectionId = getCurrentSectionId();
+      const currentHash = window.location.hash.replace("#", "");
+
+      if (currentSectionId && currentSectionId !== currentHash) {
+        setViewState(INITIAL_STATE);
+        window.history.replaceState(null, null, `#${currentSectionId}`);
+      }
+    };
+
+    window.document.body.addEventListener("scroll", handleScroll);
+
+    return () =>
+      window.document.body.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <div style={{ display: "flex" }} className="parent">
+      <div style={{ width: "25%" }}>
+        <Card1 setOutline={setOutline} />
+        <Card2 setOutline={setOutline} />
+        <Card3 setOutline={setOutline} />
+      </div>
+      <div className={styles.cardMap}>
+        <DeckGL
+          style={{ position: "relative" }}
+          viewState={viewState}
+          onViewStateChange={({ viewState }) => setViewState(viewState)}
+          layers={outline ? [new GeoJsonLayer(outline)] : []}
+          controller={DECK_GL_CONTROLLER}
+        >
+          <StaticMap
+            mapLib={maplibregl}
+            mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+          />
+        </DeckGL>
+      </div>
+    </div>
+  );
+}
