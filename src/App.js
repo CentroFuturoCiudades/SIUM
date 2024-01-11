67a497969897585e2abc60c161c63125b502baad
Adrián_Tadeo_Barrera_Almanza <101757944+TadeoBarrera@users.noreply.github.com>
2024-01-11 01:36:12 -0600

    11/01/2024

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Body from "./views/Body";
 import Title from "./views/Title";
 import Cards from "./views/Cards";
 import BodyText from "./views/BodyText";
+import DescargaDatos from "./views/DescargaDatos";
 
 function App() {
   return (
@@ -13,7 +14,8 @@ function App() {
           <Route path='/' element={ <Title/> }/> 
           <Route path='/introduccion' element={ <BodyText/> }/> 
           <Route path='/cards' element={ <Cards/> }/> 
-          <Route path='/problematica' element={ <Body/> }/> 
+          <Route path='/problematica' element={ <Body/> }/>
+          <Route path='/descarga' element={<DescargaDatos/>}/>
         </Routes>    
       </Router>
     </div>
