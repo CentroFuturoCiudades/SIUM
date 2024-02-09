19779fcf2e22fd089a03d88a82d3feeb3934e267
urielsu96 <uriels@tec.mx>
2024-02-09 17:10:58 -0600

    Leyenda de infancias

--- src/components/LegendCustom.js
+++ src/components/LegendCustom.js
@@ -1,78 +1,82 @@
-import { InfoIcon } from "@chakra-ui/icons";
-import { Box, Heading, Tooltip } from "@chakra-ui/react";
+import { Box, Table, TableContainer, Tbody, Td, Tr } from "@chakra-ui/react";
+import * as d3 from "d3";
 
 export const LegendCustom = ({
-  title,
-  info1,
   color,
-  title2,
-  legendItems,
+  pob05,
   area,
+  guarderias,
+  preescolares,
+  comercios,
+  salud,
 }) => {
-  if (!legendItems || legendItems.length === 0) {
-    return null;
-  }
+  const all = [
+    {
+      value: pob05,
+      label: "Población de 0 a 5 años",
+      color: "blue",
+      formatting: (d) => d3.format(".2f")(d) + "%",
+    },
+    {
+      value: area,
+      label: "Parques",
+      color: "rgb(0, 255, 0)",
+      formatting: (d) => d3.format(".2f")(d) + " ha",
+    },
+    {
+      value: guarderias,
+      label: "Guarderías",
+      color: "red",
+      formatting: d3.format(".0f"),
+    },
+    {
+      value: preescolares,
+      label: "Preescolar",
+      color: "orange",
+      formatting: d3.format(".0f"),
+    },
+    {
+      value: salud,
+      label: "Equipamientos de Salud",
+      color: "#7F00FF",
+      formatting: d3.format(".0f"),
+    },
+    {
+      value: comercios,
+      label: "Comercios al por menor",
+      color: "brown",
+      formatting: d3.format(".0f"),
+    },
+  ];
   return (
     <Box
       borderRadius="md"
       borderColor={`${color}.200`}
       borderWidth="0.08rem"
       className="legend-container"
-      width="200px"
     >
-      <div
-        style={{
-          width: "100%",
-          display: "flex",
-          justifyContent: "space-between",
-          alignItems: "left",
-          marginBottom: "0px",
-          marginTop: "5px",
-        }}
-      >
-        <Heading size="xs" color="gray.700">
-          {title}
-        </Heading>
-        <Tooltip
-          label="Datos obtenidos de blah blah blah"
-          placement="top"
-          hasArrow
-          gutter={12}
-        >
-          <InfoIcon
-            boxSize={3}
-            color="gray.400"
-            style={{ cursor: "pointer" }}
-            marginLeft="0.6rem"
-          />
-        </Tooltip>
-      </div>
-      <div style={{ alignItems: "center" }}>
-        <span className="legend-label">{info1}%</span>
-      </div>
-      <Heading size="xs" color="gray.700">
-        Parques
-      </Heading>
-      <div style={{ alignItems: "center" }}>
-        <span className="legend-label">{area.toFixed(2)} hectareas</span>
-      </div>
-      <Heading size="xs" color="gray.700">
-        {title2}
-      </Heading>
-      {legendItems.map((item, index) => (
-        <div key={index} style={{ display: "flex", alignItems: "center" }}>
-          <div
-            className="legend-color"
-            style={{ backgroundColor: item.color }}
-          />
-          <div className="legend-numbers">
-            <span className="legend-label" style={{ whiteSpace: "nowrap" }}>
-              {item.item1}:
-            </span>
-            <span className="legend-label">{item.item2}</span>
-          </div>
-        </div>
-      ))}
+      <TableContainer>
+        <Table size="xs" variant="unstyled">
+          <Tbody>
+            {all.map((item, index) => (
+              <Tr fontSize="0.7dvw">
+                <Td>
+                  <div
+                    className="legend-color"
+                    style={{ backgroundColor: item.color }}
+                  />
+                </Td>
+                <Td className="legend-label2">
+                  <span className="legend-label2">{item.label}</span>
+                </Td>
+                <Td fontSize="0.7dvw" isNumeric style={{ padding: "0 10px" }}>
+                  {item.formatting(item.value)}
+                </Td>
+              </Tr>
+            ))}
+          </Tbody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 };
